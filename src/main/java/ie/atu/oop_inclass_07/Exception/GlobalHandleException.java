b338e144fdb89c83d68dbbd8cf71c6e077447557
Franklin Esedebe <G00465105@atu.ie>
2026-03-04 17:02:47 +0000

    Exception Handler completed

--- src/main/java/ie/atu/oop_inclass_07/Exception/GlobalHandleException.java
+++ src/main/java/ie/atu/oop_inclass_07/Exception/GlobalHandleException.java
@@ -1,4 +1,34 @@
 package ie.atu.oop_inclass_07.Exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
 public class GlobalHandleException {
+
+    @ExceptionHandler(value={MethodArgumentNotValidException.class})
+    public ResponseEntity<Map<String, String>> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex)
+    {
+        Map<String,String> errors = new HashMap<>();
+
+        for(FieldError error : ex.getBindingResult().getFieldErrors()) {
+            errors.put(error.getField(), error.getDefaultMessage());
+        }
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+    }
+
+    @ExceptionHandler(value={IllegalArgumentException.class})
+    public ResponseEntity<Map<String, String>> handleIllegalArgumentException(IllegalArgumentException ex)
+    {
+        Map<String,String> error = new HashMap<>();
+        error.put("Error.", ex.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+    }
 }
