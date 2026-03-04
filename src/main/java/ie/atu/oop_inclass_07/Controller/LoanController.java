1bfb97f95f7e08afb0d3feaebbd93ab116ecb8fd
Franklin Esedebe <G00465105@atu.ie>
2026-03-04 16:49:46 +0000

    Service worked on and moved onto Controller

--- src/main/java/ie/atu/oop_inclass_07/Controller/LoanController.java
+++ src/main/java/ie/atu/oop_inclass_07/Controller/LoanController.java
@@ -1,12 +1,41 @@
 package ie.atu.oop_inclass_07.Controller;
 
 
+import ie.atu.oop_inclass_07.Model.Loan;
+import ie.atu.oop_inclass_07.Service.LoanService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/loans")
 public class LoanController {
+
+    private final LoanService loanService;
+    public LoanController(LoanService loanService) {
+        this.loanService = loanService;
+    }
+
+    @PostMapping("/addLoan")
+    public ResponseEntity<Loan> addLoan(@Valid @RequestBody Loan loan) {
+
+        Loan savedLoan = loanService.addLoan(loan);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedLoan);
+    }
+
+    @GetMapping("/allLoans")
+    public ResponseEntity<List<Loan>> getLoans() {
+
+    }
+
 
 
 }
